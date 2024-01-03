55c3cbc27f48f76f65c546046456c5d06e32e552
felix herrmann <felix.rodau@gmail.com>
2024-01-03 11:43:18 +0100

    add compute loss to rgb traj diff

--- policy/diffusion_transformer_image.py
+++ policy/diffusion_transformer_image.py
@@ -5,7 +5,7 @@ from utils.position_encodings import RotaryPositionEncoding2D, SinusoidalPosEmb
 from utils.clip import load_clip
 from utils.layers import RelativeCrossAttentionModule
 import einops
-from models.film import FILM
+from models.conditional_unet1d import ConditionalUnet1D
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
 
 
@@ -13,6 +13,7 @@ class DiffusionTransformerImage(nn.Module):
     def __init__(self, 
                     action_dim = 2,
                     obs_horizon = 2,
+                    action_horizon = None,
                     noise_scheduler: DDPMScheduler = DDPMScheduler(),
                     vis_backbone="clip", 
                     re_cross_attn_layer=4, 
@@ -28,6 +29,7 @@ class DiffusionTransformerImage(nn.Module):
 
         self.obs_horizon = obs_horizon
         self.action_dim = action_dim
+        self.action_horizon = action_horizon
         self.device = device
         self.embedding_dim = embedding_dim
         self.noise_scheduler = noise_scheduler
@@ -47,30 +49,15 @@ class DiffusionTransformerImage(nn.Module):
                                                                 re_cross_attn_num_heads,
                                                                 re_cross_attn_layer).to(device)
         
-        self.noise_pred_net = FILM(
-                                in_channels=action_dim,  
-                                cond_channels=embedding_dim,
-                                hidden_channels=512,
-                                diffusion_step_embed_dim=256
-                                ).to(device)
+        self.noise_pred_net = ConditionalUnet1D(input_dim=action_dim, global_cond_dim=embedding_dim)
         
         self.time_embedder = SinusoidalPosEmb(embedding_dim).to(device)
-
-        self.glob_scene_encoder = nn.Sequential(
-            nn.Linear(2*embedding_dim, 2*embedding_dim),
-            nn.ReLU(),
-            nn.Linear(2*embedding_dim, 2*embedding_dim),
-            nn.ReLU(),
-            nn.Linear(2*embedding_dim, embedding_dim),
-            nn.ReLU(),
-        )
 
         self.env_size = torch.tensor(env_size).to(self.device).float()
         self.normalize_pos_fn = lambda pos: pos / self.env_size
         self.unnormalize_pos_fn = lambda pos: pos * self.env_size
 
     def compute_visual_features(self, rgb, out_res=[24, 24]):
-        rgb = einops.rearrange(rgb, 'b t c h w -> (b t) c h w')
         with torch.no_grad():
             if out_res == [24, 24]:
                 out = self.vis_backbone(rgb)["res2"]
@@ -83,7 +70,6 @@ class DiffusionTransformerImage(nn.Module):
         out = self.vis_out_proj(out)
         out = einops.rearrange(out, 'b (h w) c -> b c h w', h=h, w=w)
         out = F.interpolate(out, size=out_res, mode='bilinear', align_corners=False)
-        out = einops.rearrange(out, '(b t) c h w -> b t c h w', t=self.obs_horizon)
         return out
     
     def get_img_positions(self, shape):
@@ -96,47 +82,68 @@ class DiffusionTransformerImage(nn.Module):
     
     def compute_context_features(self, rgb):
         rgb_features = self.compute_visual_features(rgb)
-        # (b, t, c, h, w)
-        time_embed = self.time_embedder(torch.arange(self.obs_horizon).to(self.device))
-        time_embed = einops.repeat(time_embed, 't c -> b t c h w', b=rgb_features.shape[0], h=rgb_features.shape[-2], w=rgb_features.shape[-1])
-        context_features = rgb_features + time_embed
-        context_features = einops.repeat(rgb_features, 'b t c h w -> (t h w) b c')
-        
         rgb_pos = self.get_img_positions(rgb_features.shape[-2:])
-        rgb_pos = einops.repeat(rgb_pos, 'c h w -> b (t h w) c', b=rgb_features.shape[0], t=self.obs_horizon)
+        rgb_pos = einops.repeat(rgb_pos, 'c h w -> b (h w) c', b=rgb.shape[0])
+        context_features = einops.repeat(rgb_features, 'b c h w -> (h w) b c')
         context_pos = self.rotary_embed(rgb_pos)
         return context_features, context_pos
     
     def compute_query_features(self, agent_pos):
-        # should I use the same query for all timesteps?
-        query = einops.repeat(self.query_embedding.weight, '1 c -> t b c', b=agent_pos.shape[0], t=self.obs_horizon)
-        query_pos = self.rotary_embed(agent_pos)
+        query = einops.repeat(self.query_embedding.weight, '1 c -> 1 b c', b=agent_pos.shape[0])
+        query_pos = einops.repeat(agent_pos, 'b c -> b 1 c')
+        query_pos = self.rotary_embed(query_pos)
         return query, query_pos
     
     def compute_scene_embedding(self, context_features, context_pos, query, query_pos):
-        scene_embedding = self.re_cross_attn(query=query, value=context_features, query_pos=query_pos, value_pos=context_pos)
+        scene_embeddings = self.re_cross_attn(query=query, value=context_features, query_pos=query_pos, value_pos=context_pos)
+        scene_embeddings = scene_embeddings[-1].squeeze(0)
+        scene_embeddings = einops.rearrange(scene_embeddings, '(b oh) c -> oh b c', oh=self.obs_horizon)
+        scene_embedding = self.re_cross_attn(value=scene_embeddings[:-1], query=scene_embeddings[-1:])
         scene_embedding = scene_embedding[-1].squeeze(0)
-        scene_embedding = einops.rearrange(scene_embedding, 't b c -> b (t c)')
         return scene_embedding
+
+    def normalize_rgb(self, rgb):
+        rgb = einops.rearrange(rgb, 'b t c h w -> (b t) c h w')
+        rgb = self.normalize_rgb_fn(rgb)
+        rgb = einops.rearrange(rgb, '(b t) c h w -> b t c h w', t=self.obs_horizon)
+        return rgb
+    
+    def normalize_pos(self, pos):
+        pos = self.normalize_pos_fn(pos)
+        return pos
+    
+    def unnormalize_pos(self, pos):
+        pos = self.unnormalize_pos_fn(pos)
+        return pos
+    
+    def normalize_trajectory(self, traj, agent_pos):
+        traj = traj - agent_pos[:, None, :]
+        return traj
+
+    def unnormalize_trajectory(self, traj, agent_pos):
+        traj = traj + agent_pos[:, None, :]
+        return traj
 
     def predict_action(self, obs_dict) -> torch.Tensor:
         # get observation data
-        abs_agent_pos = obs_dict["agent_pos"].to(self.device)
-        rgb = obs_dict["rgb"].to(self.device)
+        rgb = obs_dict["image"].to(self.device)
+        agent_hist = obs_dict["agent_pos"].to(self.device)
+        agent_pos = agent_hist[:, -1]
         B = rgb.shape[0]
         
         # normalize data
-        nrgb = self.normalize_rgb(rgb)
-        nagent_pos = self.normalize_pos(abs_agent_pos)
+        nrgb = self.normalize_rgb_fn(rgb)
+        agent_hist = self.normalize_pos(agent_hist)
 
+        # compute scene embedding
+        nrgb = einops.rearrange(nrgb, 'b oh c h w -> (b oh) c h w')
+        agent_hist = einops.rearrange(agent_hist, 'b oh c -> (b oh) c')
         context_features, context_pos = self.compute_context_features(nrgb)
-        query, query_pos = self.compute_query_features(nagent_pos)
-        scene_embeddings = self.compute_scene_embedding(context_features, context_pos, query, query_pos)
-        # should maybe be replaced by transformer or find different way to aggregate
-        scene_embedding = self.glob_scene_encoder(scene_embeddings)
+        query, query_pos = self.compute_query_features(agent_hist)
+        scene_embedding = self.compute_scene_embedding(context_features, context_pos, query, query_pos)
 
         noisy_action = torch.randn(
-            (B, self.action_dim), device=self.device)
+            (B, self.action_horizon, self.action_dim), device=self.device)
         naction = noisy_action
 
         self.noise_scheduler.set_timesteps(num_inference_steps=self.num_inference_steps, device=self.device)
@@ -154,54 +161,42 @@ class DiffusionTransformerImage(nn.Module):
                 sample=naction
             ).prev_sample
 
-        action = naction + nagent_pos
-        action = self.unnormalize_pos(action)
+        action = self.unnormalize_pos(naction)
         return action
-    
-    def normalize_rgb(self, rgb):
-        rgb = einops.rearrange(rgb, 'b t c h w -> (b t) c h w')
-        rgb = self.normalize_rgb_fn(rgb)
-        rgb = einops.rearrange(rgb, '(b t) c h w -> b t c h w', t=self.obs_horizon)
-        return rgb
-    
-    def normalize_pos(self, pos):
-        pos = self.normalize_pos_fn(pos)
-        return pos
-    
-    def unnormalize_pos(self, pos):
-        pos = self.unnormalize_pos_fn(pos)
-        return pos
+
 
     def compute_loss(self, batch):
         # get batch data
-        rgb = batch["rgb"].to(self.device)
-        abs_agent_pos = batch["agent_pos"].to(self.device)
-        abs_goal_pos = batch["goal_pose"][...,:2].to(self.device)
+        rgb = batch["image"].to(self.device)
+        agent_hist = batch["agent_pos"].to(self.device)
+        agent_pos = agent_hist[:, -1]
+        traj = batch["action"].to(self.device)
         B = rgb.shape[0]
 
         # normalize data
         nrgb = self.normalize_rgb_fn(rgb)
-        nagent_pos = self.normalize_pos(abs_agent_pos)
-        ngoal_pos = self.normalize_pos(abs_goal_pos)
+        agent_hist = self.normalize_pos(agent_hist)
+        traj = self.normalize_pos(traj)
+        traj = self.normalize_trajectory(traj, agent_pos)
 
+        # compute scene embedding
+        nrgb = einops.rearrange(nrgb, 'b oh c h w -> (b oh) c h w')
+        agent_hist = einops.rearrange(agent_hist, 'b oh c -> (b oh) c')
         context_features, context_pos = self.compute_context_features(nrgb)
-        query, query_pos = self.compute_query_features(nagent_pos)
-        scene_embeddings = self.compute_scene_embedding(context_features, context_pos, query, query_pos)
-        # should maybe be replaced by transformer or find different way to aggregate
-        scene_embedding = self.glob_scene_encoder(scene_embeddings)
+        query, query_pos = self.compute_query_features(agent_hist)
+        scene_embedding = self.compute_scene_embedding(context_features, context_pos, query, query_pos)
 
         # add noise to target
-        goal_pos = ngoal_pos[:,-1] - nagent_pos[:,-1]
-        noise = torch.randn(goal_pos.shape).to(self.device)
+        noise = torch.randn(traj.shape).to(self.device)
         timesteps = torch.randint(
             0, self.noise_scheduler.config.num_train_timesteps, 
-            (goal_pos.shape[0],), device=goal_pos.device
+            (B,), device=traj.device
         ).long()
-        noisy_goal_pos = self.noise_scheduler.add_noise(
-            goal_pos, noise, timesteps)
+        noisy_traj = self.noise_scheduler.add_noise(
+            traj, noise, timesteps)
         
-        pred = self.noise_pred_net(sample=noisy_goal_pos, timestep=timesteps, global_cond=scene_embedding)
-        loss = F.mse_loss(pred, noise).mean()
+        noise_pred = self.noise_pred_net(sample=noisy_traj, timestep=timesteps, global_cond=scene_embedding)
+        loss = F.mse_loss(noise_pred, noise).mean()
 
         return loss
     
